0deb6425fb0e63b58c46f5bedbc3c19f51b69dc8
ozkansari <ozkansari@gmail.com>
2021-03-01 17:31:12 +0300

    Delete WebMebisSpring directory

--- WebMebisSpring/src/main/java/tr/edu/medipol/ilerijava/WebMebisSpring/OgrenciRepository.java
+++
@@ -1,9 +0,0 @@
-package tr.edu.medipol.ilerijava.WebMebisSpring;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OgrenciRepository extends JpaRepository<Ogrenci, Long> {
-
-}
